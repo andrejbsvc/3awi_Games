5eff054503386fa05b8109aace36db1795b6db86
Your Name <you@example.com>
2025-03-07 13:22:09 +0100

    Aufgabe 01 Rectangle DONE

--- src/at/andrej/Games_Programmieren/games/firstgame/RectanglesGame.java
+++ src/at/andrej/Games_Programmieren/games/firstgame/RectanglesGame.java
@@ -5,28 +5,33 @@ import org.newdawn.slick.tests.DeferredLoadingTest;
 
 public class RectanglesGame extends BasicGame {
 
+    private int x;
+
     public RectanglesGame(){
         super("Rectangles");
     }
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
 
+        this.x = 100;
     }
 
     @Override
     public void update(GameContainer gameContainer, int i) throws SlickException {
 
+        this.x++;
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-
+        graphics.drawRect(this.x, 100, 100, 100);
+        graphics.drawString("Hello you", 50, 50);
     }
 
     public static void main(String[] argv) {
         try {
             AppGameContainer container = new AppGameContainer(new RectanglesGame());
-            container.setDisplayMode(800,600,false);
+            container.setDisplayMode(1200,700,false);
             container.start();
         } catch (SlickException e) {
             e.printStackTrace();
