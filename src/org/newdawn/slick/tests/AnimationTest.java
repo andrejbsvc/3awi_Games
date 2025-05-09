e970737c3902538e3272492ab14e508ca7fca91a
Your Name <you@example.com>
2025-05-09 12:00:13 +0200

    Aufgabe DONE

--- src/org/newdawn/slick/tests/AnimationTest.java
+++ src/org/newdawn/slick/tests/AnimationTest.java
@@ -100,7 +100,7 @@ public class AnimationTest extends BasicGame {
 	public static void main(String[] argv) {
 		try {
 			AppGameContainer container = new AppGameContainer(new AnimationTest());
-			container.setDisplayMode(800,600,false);
+			container.setDisplayMode(800, 600, false);
 			container.start();
 		} catch (SlickException e) {
 			e.printStackTrace();
